153be7e727c5ed1fe0faa0fcf6accfd2845b0a62
Bri Dickman <brianna.dickman@gmail.com>
2017-08-11 23:10:58 -0500

    base of app made, added in swipe gesture code example

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -5,10 +5,16 @@ import { NavController } from 'ionic-angular';
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
   }
+  
+  public swipe: number = 0;
 
+  swipeEvent(e) {
+    this.swipe++
+   }
 }
