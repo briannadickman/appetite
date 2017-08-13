15e39b94264fdee393694928e15573078d78b306
Bri Dickman <brianna.dickman@gmail.com>
2017-08-13 11:29:55 -0500

    added in firebase and login page

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyCtt1iZaxxQpwXt8tt4fIvRnw81qHcqyfo",
+    authDomain: "appetite-ca7a9.firebaseapp.com",
+    databaseURL: "https://appetite-ca7a9.firebaseio.com",
+    projectId: "appetite-ca7a9",
+    storageBucket: "appetite-ca7a9.appspot.com",
+    messagingSenderId: "1088879784153"
+  };
